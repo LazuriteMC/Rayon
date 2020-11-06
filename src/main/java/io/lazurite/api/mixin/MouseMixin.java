3832012c290a84047d6017879ecf0a2a02cd40cb
Ethan Johnson <ethan@ejohnsons.com>
2020-11-05 20:51:07 -0600

    Fixed MouseMixin

--- src/main/java/io/lazurite/api/mixin/MouseMixin.java
+++ src/main/java/io/lazurite/api/mixin/MouseMixin.java
@@ -1,10 +1,13 @@
 package io.lazurite.api.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -15,6 +18,8 @@ import org.spongepowered.asm.mixin.injection.Redirect;
  */
 @Mixin(Mouse.class)
 public class MouseMixin {
+    @Shadow @Final private MinecraftClient client;
+
     /**
      * This mixin redirects the {@link ClientPlayerEntity#changeLookDirection(double, double)} method
      * so that when the mouse is moved while the camera is being manipulated, nothing happens.
@@ -22,7 +27,7 @@ public class MouseMixin {
      * @param cursorDeltaX the x cursor position
      * @param cursorDeltaY the y cursor position
      */
-    /*@Redirect(
+    @Redirect(
             method = "updateMouse",
             at = @At(
                     value = "INVOKE",
@@ -30,10 +35,10 @@ public class MouseMixin {
             )
     )
     public void changeLookDirection(ClientPlayerEntity player, double cursorDeltaX, double cursorDeltaY) {
-        if (!GogglesItem.isInGoggles()) {
+        if (client.getCameraEntity() == client.player) {
             player.changeLookDirection(cursorDeltaX, cursorDeltaY);
         }
-    }*/
+    }
 
     /**
      * This mixin redirects the {@link KeyBinding#setKeyPressed(InputUtil.Key, boolean)} method
@@ -41,7 +46,7 @@ public class MouseMixin {
      * @param key the key being pressed
      * @param pressed whether or not the key is pressed
      */
-    /*@Redirect(
+    @Redirect(
             method = "onMouseButton",
             at = @At(
                     value = "INVOKE",
@@ -49,15 +54,15 @@ public class MouseMixin {
             )
     )
     public void setKeyPressed(InputUtil.Key key, boolean pressed) {
-        KeyBinding.setKeyPressed(key, !GogglesItem.isInGoggles() && pressed);
-    }*/
+        KeyBinding.setKeyPressed(key, client.getCameraEntity() == client.player && pressed);
+    }
 
     /**
      * This mixin redirects the {@link KeyBinding#onKeyPressed(InputUtil.Key)} method
      * so that when the player is manipulating the camera, it is not called.
      * @param key the key being pressed
      */
-    /*@Redirect(
+    @Redirect(
             method = "onMouseButton",
             at = @At(
                     value = "INVOKE",
@@ -65,8 +70,8 @@ public class MouseMixin {
             )
     )
     public void onKeyPressed(InputUtil.Key key) {
-        if (!GogglesItem.isInGoggles()) {
+        if (client.getCameraEntity() == client.player) {
             KeyBinding.onKeyPressed(key);
         }
-    }*/
+    }
 }
