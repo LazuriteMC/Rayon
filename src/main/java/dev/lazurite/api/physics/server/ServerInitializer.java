83bf9fb2b61acfe27de680aa57e798bb3874bf50
Ethan Johnson <ethan@ejohnsons.com>
2020-11-24 21:49:56 -0600

    Changed mod id

--- src/main/java/dev/lazurite/api/physics/server/ServerInitializer.java
+++ src/main/java/dev/lazurite/api/physics/server/ServerInitializer.java
@@ -4,7 +4,7 @@ import dev.lazurite.api.physics.network.packet.PhysicsHandlerC2S;
 import net.fabricmc.api.ModInitializer;
 
 public class ServerInitializer implements ModInitializer {
-	public static final String MODID = "lazurite-physics-api";
+	public static final String MODID = "lazurite-physics";
 	public static final String VERSION = "1.0.0";
 	public static final String URL = "https://github.com/LazuriteMC/Lazurite-Physics-API/releases";
 
