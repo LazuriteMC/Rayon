07f28d1724ad9782b994624a593cde7c5f51e097
Ethan Johnson <ethan@ejohnsons.com>
2020-11-30 23:30:48 -0600

    Documented ClientInitializer

--- example_mods/entity_example/src/main/java/dev/lazurite/rayon/entity_example/client/ClientInitializer.java
+++ example_mods/entity_example/src/main/java/dev/lazurite/rayon/entity_example/client/ClientInitializer.java
@@ -1,13 +1,25 @@
 package dev.lazurite.rayon.entity_example.client;
 
 import dev.lazurite.rayon.entity_example.client.render.TestEntityRenderer;
+import dev.lazurite.rayon.entity_example.server.ServerInitializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
+/**
+ * This is the client-side entrypoint for this mod. It normally
+ * contains registries for things like renderers, keybindings, etc.
+ * @author Ethan Johnson
+ */
 @Environment(EnvType.CLIENT)
 public class ClientInitializer implements ClientModInitializer {
 
+    /**
+     * Similar to {@link ServerInitializer#onInitialize()}, this
+     * method is where you can put any code you want to run when
+     * the mod is loaded just on the client. In this case, we're
+     * registering the {@link TestEntityRenderer}.
+     */
     @Override
     public void onInitializeClient() {
         TestEntityRenderer.register();
