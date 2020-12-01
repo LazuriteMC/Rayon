5236f60ebd730b2ec9f9ba5b8de7c40b5f571858
Ethan Johnson <ethan@ejohnsons.com>
2020-11-30 23:28:12 -0600

    Documented ServerInitializer

--- example_mods/entity_example/src/main/java/dev/lazurite/rayon/entity_example/server/ServerInitializer.java
+++ example_mods/entity_example/src/main/java/dev/lazurite/rayon/entity_example/server/ServerInitializer.java
@@ -9,11 +9,22 @@ import net.minecraft.entity.SpawnGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
+/**
+ * This is the server-side entrypoint for this mod. It normally
+ * contains registries for things like entities, blocks, and items.
+ * @author Ethan Johnson
+ */
 public class ServerInitializer implements ModInitializer {
+
     public static final String MODID = "rayon_entity_example";
 
+    /** The new entity type for our {@link TestEntity} class. */
     public static EntityType<TestEntity> TEST_ENTITY;
 
+    /**
+     * Anything you want to run when the mod is loaded should go here.
+     * In this case, we create our {@link EntityType<TestEntity>} object.
+     */
     @Override
     public void onInitialize() {
         TEST_ENTITY = Registry.register(
