81cbf2de6ac187a117090cedacf5273bc66082e3
Ethan Johnson <ethan@ejohnsons.com>
2021-01-24 13:40:33 -0600

    Moved mod menu entrypoint so it no longer has to be included in the jar

---
+++ src/main/java/dev/lazurite/rayon/util/config/ModMenuEntry.java
@@ -0,0 +1,16 @@
+package dev.lazurite.rayon.util.config;
+
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
+
+public class ModMenuEntry implements ModMenuApi {
+    /**
+     * Adds the config screen mod menu.
+     * @return the {@link ConfigScreenFactory}
+     * @see ConfigScreen
+     */
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::create;
+    }
+}
