b76e28b1de0eab2b69e37e200e3318ccc488fda0
ethanejohnsons <ethan@ejohnsons.com>
2021-02-18 21:14:46 -0600

    Downgraded mod menu and cloth because they're fubar at the moment

--- src/main/java/dev/lazurite/rayon/impl/util/config/ConfigScreen.java
+++ src/main/java/dev/lazurite/rayon/impl/util/config/ConfigScreen.java
@@ -1,7 +1,7 @@
 package dev.lazurite.rayon.impl.util.config;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
 import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import me.shedaniel.clothconfig2.api.ConfigCategory;
 import net.fabricmc.api.EnvType;
