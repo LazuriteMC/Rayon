9d84dd3c91f9f687449b57ec9440274f59dff47e
ethanejohnsons <ethan@ejohnsons.com>
2023-04-13 19:14:52 -0500

    Added rayon server detection

--- common/src/main/java/dev/lazurite/rayon/impl/Rayon.java
+++ common/src/main/java/dev/lazurite/rayon/impl/Rayon.java
@@ -2,16 +2,21 @@ package dev.lazurite.rayon.impl;
 
 import dev.lazurite.rayon.impl.bullet.natives.NativeLoader;
 import dev.lazurite.rayon.impl.bullet.thread.PhysicsThread;
+import dev.lazurite.rayon.impl.bullet.thread.util.ClientUtil;
 import dev.lazurite.rayon.impl.event.ClientEventHandler;
 import dev.lazurite.rayon.impl.event.ServerEventHandler;
 import dev.lazurite.rayon.impl.event.network.EntityNetworking;
+import dev.lazurite.toolbox.api.network.PacketRegistry;
 import dev.lazurite.transporter.impl.Transporter;
+import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class Rayon {
 	public static final String MODID = "rayon";
 	public static final Logger LOGGER = LogManager.getLogger("Rayon");
+
+	private static boolean serverHasRayon = false;
 
 	public static void intialize() {
 		// prevent annoying libbulletjme spam
@@ -26,9 +31,21 @@ public class Rayon {
 	public static void initializeClient() {
 		ClientEventHandler.register();
 		EntityNetworking.registerClient();
+
+		// Rayon Server Detection
+		PacketRegistry.registerClientbound(new ResourceLocation(MODID, "i_have_rayon"), ctx -> serverHasRayon = true);
 	}
 
 	public static PhysicsThread getThread(boolean isClient) {
 		return isClient ? ClientEventHandler.getThread() : ServerEventHandler.getThread();
+	}
+
+	public static boolean serverHasRayon() {
+		var isConnected = ClientUtil.isConnectedToServer();
+		if (!isConnected) {
+			serverHasRayon = false;
+			return false;
+		}
+		return serverHasRayon;
 	}
 }
--- forge/src/main/java/dev/lazurite/rayon/impl/bullet/thread/util/forge/ClientUtilImpl.java
+++ forge/src/main/java/dev/lazurite/rayon/impl/bullet/thread/util/forge/ClientUtilImpl.java
@@ -1,15 +1,10 @@
 package dev.lazurite.rayon.impl.bullet.thread.util.forge;
 
-import net.minecraft.client.Minecraft;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 public class ClientUtilImpl {
-    public static boolean isPaused() {
-        if (FMLLoader.getDist() == Dist.CLIENT) {
-            return Minecraft.getInstance().isPaused();
-        }
-
-        return false;
+    public static boolean isClient() {
+        return FMLLoader.getDist() == Dist.CLIENT;
     }
 }
